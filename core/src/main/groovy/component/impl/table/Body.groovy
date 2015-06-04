8858dce422643dcf3d2e268b0e61a4e269e4996c
assilzm <assilzm@gmail.com>
2015-06-04 11:30:36 +0800

    增加列头及表体类

---
+++ core/src/main/groovy/component/impl/table/Body.groovy
@@ -0,0 +1,13 @@
+package component.impl.table
+
+import base.model.BasicPage
+
+/**
+ * 表体
+ */
+class Body extends BasicPage{
+    /**
+     * 表体表即行的集合
+     */
+    List<Row> rows=new ArrayList<>()
+}
---
+++ core/src/main/groovy/component/impl/table/Head.groovy
@@ -0,0 +1,11 @@
+package component.impl.table
+
+import base.model.BasicPage
+
+/**
+ * 列头
+ */
+class Head extends BasicPage{
+
+    List<Cell> Cells=new ArrayList<>()
+}
